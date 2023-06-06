dc6d2a9a0c98a6f298b72d707265c002cf08c85f
prabin.sc1 <prabin.sc1@gmail.com>
2023-06-06 09:58:39 +0545

    second commit

---
+++ testingcollaboration/src/main/java/com/tc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.tc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("hello");
+		return "home";
+	}
+
+}
